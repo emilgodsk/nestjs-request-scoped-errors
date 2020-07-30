74f9e1ad3ad3ca66db0949921ac55fd9e9ff8ddb
Emil Godsk <emil.godsk@gmail.com>
2020-07-30 19:26:16 +0200

    initial

---
+++ src/main.ts
@@ -0,0 +1,91 @@
+import {
+  Catch,
+  Controller,
+  ExceptionFilter,
+  Get, HttpException,
+  Inject,
+  Module,
+  Scope,
+} from '@nestjs/common';
+import { BaseExceptionFilter, NestFactory, REQUEST } from '@nestjs/core';
+
+@Controller('app')
+export class AppController {
+  constructor(
+    @Inject('CONNECTION')
+    private readonly connection
+  ) {}
+
+  @Get()
+  async get() {
+    console.log(this.connection);
+
+    return true;
+  }
+}
+
+@Module({
+  imports: [],
+  providers: [
+    {
+      provide: 'CONNECTION',
+      scope: Scope.REQUEST,
+      inject: [REQUEST],
+      useFactory: async (request: Request) => {
+        console.log('connectionFactory - useFactory - 1');
+        throw new CustomException();
+      },
+    }
+  ],
+  exports: ['CONNECTION'],
+})
+export class ConnectionModule {}
+
+@Module({
+  imports: [
+    ConnectionModule,
+  ],
+  controllers: [AppController],
+})
+export class AppModule {}
+
+export class CustomException extends Error {
+  constructor() {
+    super('CUSTOM EXCEPTION');
+  }
+}
+
+@Catch(CustomException)
+export class CustomExceptionFilter implements ExceptionFilter {
+  public catch(): any {
+    console.log('CustomExceptionFilter - catch - 1');
+
+    // throwing an error here return in an "UnhandledPromiseRejectionWarning" in the console, and response is never sent.
+    // throw new Error('test');
+
+    // throwing an HttpException here return in an "UnhandledPromiseRejectionWarning" in the console, and response is never sent.
+    throw new HttpException('test', 400);
+
+    // returning a custom error object results in the response never getting terminated
+    // return new Error('custom error');
+  }
+}
+
+// This fails as well.
+//
+// @Catch(CustomException)
+// export class CustomExceptionFilter extends BaseExceptionFilter {
+//   public catch(exception, host): any {
+//     console.log('CustomExceptionFilter - catch - 2');
+//
+//     // Extending the BaseExceptionFilter and just calling super results in "UnhandledPromiseRejectionWarning: TypeError: Cannot read property 'reply' of undefined"
+//     super.catch(exception, host);
+//   }
+// }
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new CustomExceptionFilter());
+  await app.listen(3000);
+}
+bootstrap();
